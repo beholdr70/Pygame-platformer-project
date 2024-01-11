f4075efce51d7260ac20d3e2739ba9c3af2ca015
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 22:59:10 +0400

    Too many changes

---
+++ levels/TSX/Pixel Platformer Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="main_lev_buildA" tilewidth="16" tileheight="16" tilecount="7056" columns="84">
+ <image source="../levels_assets/PixelPlatformerSet2v.1.1/main_lev_buildA.png" width="1344" height="1344"/>
+</tileset>
