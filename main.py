2ca91dfb9c1ef0a0dab1b8ce7f60e098da7ed6a8
BuildTools <unconfigured@null.spigotmc.org>
2024-01-20 01:25:31 +0400

    Options added

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ FPS = 60
 # Setup Variables
 # Menu Related Variables
 background = menu.background
+pause_surf = pygame.Surface((size[0] * 0.55, size[1] * 0.65))
+pause_surf.fill('royalblue4')
+pause_surf.set_alpha(75)
 
 #  Level Related Variables
 current_level, spawnpoint, platform_group, decor_back_group, decor_front_group = None, None, None, None, None
@@ -97,13 +100,32 @@ def load_settings():
         screen = pygame.display.set_mode(settings['screen_size'], flags, vsync=1)
 
     # changing the sound settings
+    for channel in [ambience_channel, music_channel, menu.menu_sfx_channel]:
+        channel.stop()
     music_channel.set_volume(settings['music_volume'])
     ambience_channel.set_volume(settings['sfx_volume'])
+    menu.menu_sfx_channel.set_volume(settings['sfx_volume'])
     if player_group:
         list(player_group)[0].SFX.set_volume(settings['sfx_volume'])
 
+    # options sync
+    menu.sfx_slider.set_position(settings['sfx_volume'])
+    menu.music_slider.set_position(settings['music_volume'])
+    if settings['fullscreen']:
+        menu.fullscreen_checkbox_button.text_change('On')
+    menu.resolution_button.text_change(f'{settings["screen_size"]}')
+
 
 load_settings()
+
+
+def save_settings(settings_dict):
+    file = open('config', 'w')
+    end_res = ''
+    for name, value in ([i, list(settings.values())[list(settings.keys()).index(i)]] for i in settings.keys()):
+        end_res += f'{name} = {value}\n'
+    file.write(end_res.strip())
+    file.close()
 
 
 # Exit Functions
@@ -176,6 +198,7 @@ def draw_background():
 if __name__ == '__main__':
     pause = False
     while True:
+        mouse_pos = pygame.mouse.get_pos()
 
         # event loop
         for event in pygame.event.get():
@@ -186,19 +209,41 @@ if __name__ == '__main__':
                 if event.key == pygame.K_r:
                     load_settings()
                 # pause the game
-                if event.key == pygame.K_ESCAPE and not menu.menu_state:
+                if event.key == pygame.K_ESCAPE and not menu.menu_state and not menu.options_state:
                     pause = not pause
 
-            if menu.menu_state:
+            if (menu.menu_state or pause) and menu.options_state:
+                menu.back_button.handle_event(event, 'BACK')
+                boolean = menu.fullscreen_checkbox_button.handle_event(event, 'CHECKBOX')
+                settings['fullscreen'] = boolean if boolean != None else settings['fullscreen']
+                menu.resolution_button.handle_event(event, 'SWITCH')
+                settings['sfx_volume'] = menu.sfx_slider.handle_event(event, mouse_pos, settings['screen_size'])
+                settings['music_volume'] = menu.music_slider.handle_event(event, mouse_pos, settings['screen_size'])
+                if menu.apply_button.handle_event(event, 'APPLY'):
+                    settings['sfx_volume'] = 1 if settings['sfx_volume'] > 0.95 else settings['sfx_volume']
+                    settings['music_volume'] = 0 if settings['music_volume'] < 0.05 else settings['music_volume']
+                    settings['screen_size'] = eval(menu.resolution_button.text)
+                    save_settings(settings)
+                    load_settings()
+
+            elif menu.menu_state:
 
                 # activating buttons
-                if menu.play_button.handle_event(event, 'START'):
-                    music_channel.stop()
-                    ambience_channel.stop()
-                    setup()
-                    cutscene = True
-                menu.quit_button.handle_event(event, 'QUIT')
-                menu.options_button.handle_event(event, 'OPTIONS')
+                if not menu.options_state:
+                    if menu.play_button.handle_event(event, 'START'):
+                        music_channel.stop()
+                        ambience_channel.stop()
+                        setup()
+                        cutscene = True
+                    menu.quit_button.handle_event(event, 'QUIT')
+                    menu.options_button.handle_event(event, 'OPTIONS')
+                    menu.level_reset_button.handle_event(event, 'RESET')
+
+            elif pause:
+                if menu.resume_button_pause.handle_event(event, 'RESUME'):
+                    pause = False
+                menu.quit_button_pause.handle_event(event, 'QUIT')
+                menu.options_button_pause.handle_event(event, 'OPTIONS')
 
         if not pause:
             if not menu.menu_state:
@@ -248,14 +293,6 @@ if __name__ == '__main__':
                         if player.rect.center[0] not in range(size[0]) or player.rect.center[1] not in range(size[1]):
                             player.rect.topleft = (spawnpoint[0], spawnpoint[1] - 80)
 
-            # display update
-            display.fill((0, 0, 0))
-            draw_background()
-
-            if not menu.menu_state:
-
-                decor_back_group.draw(display)
-
                 # changing transparency of objects in hint group to hide/show them
                 if not cutscene:
                     if pygame.sprite.spritecollide(player, hint_group, False):
@@ -269,26 +306,49 @@ if __name__ == '__main__':
                 for hint in hint_group:
                     hint.image.set_alpha(hint_alpha)
 
-                hint_group.draw(display)
-                player_group.draw(display)
-                platform_group.draw(display)
-                decor_front_group.draw(display)
+        # display update
 
-                # Fading into the level at the start
-                if cutscene:
-                    display.blit(fade_in_out, (0, 0))
-            else:
-                parallax_background_offset += 0.5
-                display.blit(menu.logo, (15, 15))
-                for button in [menu.options_button, menu.play_button, menu.quit_button]:
-                    button.check_hover(pygame.mouse.get_pos())
-                    button.draw(display)
-            screen.blit(pygame.transform.scale(display, settings['screen_size']), (0, 0))
-            pygame.display.update()
+        display.fill((0, 0, 0))
+        draw_background()
 
+        if not menu.menu_state:
+
+            decor_back_group.draw(display)
+            hint_group.draw(display)
+            player_group.draw(display)
+            platform_group.draw(display)
+            decor_front_group.draw(display)
+
+            # Fading into the level at the start
+            if cutscene:
+                display.blit(fade_in_out, (0, 0))
         else:
-            # !PAUSE MENU CODE GOES HERE (TBA)!
-            pass
+            parallax_background_offset += 0.5
+            display.blit(menu.logo, (15, 15))
+            if not menu.options_state:
+                for button in menu.menu_buttons_group:
+                    button.check_hover(mouse_pos, settings['screen_size'])
+                    button.draw(display)
+                if int(open('save_data').read()[-1]) > 1:
+                    menu.level_reset_button.check_hover(mouse_pos, settings['screen_size'])
+                    menu.level_reset_button.draw(display)
+        if pause:
+            display.blit(pause_surf, (camera_rect.center[0] - pause_surf.get_width() // 2,
+                                      camera_rect.center[1] - pause_surf.get_height() // 2))
+            if not menu.options_state:
+                for button in menu.pause_buttons_group:
+                    button.check_hover(mouse_pos, settings['screen_size'])
+                    button.draw(display)
+        if menu.options_state:
+            for button in menu.option_buttons_group:
+                if type(button) != tuple:
+                    button.check_hover(mouse_pos, settings['screen_size'])
+                    button.draw(display)
+                else:
+                    menu.draw_outline(button[0], button[1], display, 'gray10')
+                    display.blit(button[0], button[1])
+        screen.blit(pygame.transform.scale(display, settings['screen_size']), (0, 0))
+        pygame.display.update()
 
         # sound update
         if not music_channel.get_busy():
