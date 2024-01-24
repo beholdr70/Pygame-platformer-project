4d068c689171d347140a15d4cd086ae595a2fcdf
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 17:11:27 +0400

    Final Level Background remade

---
+++ levels/TSX/Background Dawn.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Background Dawn" tilewidth="576" tileheight="324" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="576" height="324" source="../levels_assets/Clouds 7/1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="576" height="324" source="../levels_assets/Clouds 7/2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="576" height="324" source="../levels_assets/Clouds 7/3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="576" height="324" source="../levels_assets/Clouds 7/4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../levels_assets/Sun/Sprite-0002.png"/>
+ </tile>
+ <tile id="5">
+  <image width="576" height="324" source="../levels_assets/Clouds 4/1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="576" height="324" source="../levels_assets/Clouds 4/2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="576" height="324" source="../levels_assets/Clouds 4/3.png"/>
+ </tile>
+ <tile id="8">
+  <image width="576" height="324" source="../levels_assets/Clouds 4/4.png"/>
+ </tile>
+</tileset>
